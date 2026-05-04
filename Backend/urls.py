9902814b777c82ad74a90bffdf52b660c1d4e15c
vaithiyanathan21 <svaithiyanathan46@gmail.com>
2026-05-04 12:59:42 +0530

    Add files via upload

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,29 +1,28 @@
-from django.urls import path
-from .views import (
-    UserViewSet, CategoryViewSet, QuizViewSet,
-    QuestionViewSet, OptionViewSet, ResultViewSet,
-    login_view, register_view
-)
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-
-    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('categories/', CategoryViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('categories/<int:pk>/', CategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('quizzes/', QuizViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('quizzes/<int:pk>/', QuizViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('questions/', QuestionViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('questions/<int:pk>/', QuestionViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('options/', OptionViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('options/<int:pk>/', OptionViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-
-    path('results/', ResultViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('results/<int:pk>/', ResultViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-]
+    path('admin/', admin.site.urls),
+    path('myapp/', include('myapp.urls')),
+   
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
